de1d9ff21c8123b0c0b65a0a12aa205b7b349cc3
roynatech2544 <whiteshell2544@naver.com>
2023-10-20 18:34:33 +0900

    r3q: Include rootdir scripts

--- device.mk
+++ device.mk
@@ -15,3 +15,21 @@ PRODUCT_AAPT_PREF_CONFIG := xxhdpi
 
 PRODUCT_SOONG_NAMESPACES += \
      $(DEVICE_PATH)
+
+# Rootdir
+PRODUCT_PACKAGES += \
+    init.class_main.sh \
+    init.qcom.class_core.sh \
+    init.qcom.early_boot.sh \
+    init.qcom.post_boot.sh \
+    init.qcom.sh \
+    init.qcom.usb.sh
+
+PRODUCT_PACKAGES += \
+    fstab.qcom \
+    init.qcom.rc \
+    init.qcom.usb.rc \
+    init.r3q.rc \
+    init.samsung.display.rc \
+    init.samsung.rc \
+    init.target.rc
