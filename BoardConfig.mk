fe6f11a8deae6b34ec6dd50c5c3ff5a9ffac5dfb
roynatech2544 <whiteshell2544@naver.com>
2023-10-20 19:18:41 +0900

    r3q: Setup kernel build

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -29,6 +29,23 @@ BOARD_USES_QCOM_HARDWARE := true
 TARGET_BOARD_PLATFORM := msmnile
 BOARD_VENDOR := samsung
 
+# Kernel
+BOARD_BOOTIMG_HEADER_VERSION := 1
+BOARD_KERNEL_BASE := 0x00000000
+BOARD_KERNEL_CMDLINE := console=null androidboot.hardware=qcom androidboot.console=ttyMSM0 androidboot.memcg=1 lpm_levels.sleep_disabled=1 video=vfb:640x400,bpp=32,memsize=3072000 msm_rtb.filter=0x237 service_locator.enable=1 swiotlb=2048 androidboot.usbcontroller=a600000.dwc3 firmware_class.path=/vendor/firmware_mnt/image loop.max_part=7
+BOARD_KERNEL_PAGESIZE := 4096
+BOARD_RAMDISK_OFFSET := 0x02000000
+BOARD_KERNEL_TAGS_OFFSET := 0x01e00000
+BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOTIMG_HEADER_VERSION)
+BOARD_MKBOOTIMG_ARGS += --ramdisk_offset $(BOARD_RAMDISK_OFFSET)
+BOARD_MKBOOTIMG_ARGS += --tags_offset $(BOARD_KERNEL_TAGS_OFFSET)
+BOARD_INCLUDE_DTB_IN_BOOTIMG := true
+BOARD_KERNEL_IMAGE_NAME := Image-dtb
+BOARD_KERNEL_SEPARATED_DTBO := true
+
+TARGET_KERNEL_CONFIG := r3q_defconfig
+TARGET_KERNEL_ARCH := arm64
+
 # Display
 TARGET_SCREEN_DENSITY := 420
 
