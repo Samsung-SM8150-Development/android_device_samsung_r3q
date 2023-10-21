dfde663dd19d48b6ac5d66a0f220da8210a03431
LuK1337 <priv.luk@gmail.com>
2023-10-21 15:32:08 +0200

    r3q: Shim Samsung keymaster HAL impl with libcrypto-v33.so
    Current asn1t.h structs aren't binary compatible with our keymaster.
    
    Change-Id: Id78b71c1f08216f861a033097cd015ba3c109925

--- device.mk
+++ device.mk
@@ -448,6 +448,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.vibrator@1.3.vendor
 
+# VNDK
+PRODUCT_COPY_FILES += \
+    prebuilts/vndk/v33/arm64/arch-arm64-armv8-a/shared/vndk-core/libcrypto.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libcrypto-v33.so
+
 # WiFi
 PRODUCT_PACKAGES += \
     android.hardware.wifi-service \
