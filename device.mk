c8eb08b3f7ef5f3c1e982d26e35adff4e1872437
roynatech2544 <whiteshell2544@naver.com>
2023-10-21 20:07:14 +0900

    r3q: Fix HWC build

--- device.mk
+++ device.mk
@@ -72,12 +72,14 @@ PRODUCT_PACKAGES += \
 
 # Display
 PRODUCT_PACKAGES += \
+    android.hardware.graphics.composer@2.4-service \
     android.hardware.graphics.mapper@3.0-impl-qti-display \
     android.hardware.graphics.mapper@4.0-impl-qti-display \
     android.hardware.memtrack@1.0-impl \
     android.hardware.memtrack@1.0-service
 
 PRODUCT_PACKAGES += \
+    hwcomposer.msmnile \
     memtrack.msmnile \
     gralloc.msmnile
 
@@ -98,7 +100,6 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_PACKAGES += \
     vendor.qti.hardware.display.allocator-service \
-    vendor.qti.hardware.display.composer-service \
     vendor.qti.hardware.display.allocator@3.0.vendor \
     vendor.qti.hardware.display.allocator@4.0.vendor \
     vendor.qti.hardware.display.composer@2.0.vendor \
