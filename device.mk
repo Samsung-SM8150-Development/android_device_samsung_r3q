e2779fc007ad32b1da48f2b9cc6afd643e413ca1
roynatech2544 <whiteshell2544@naver.com>
2023-11-07 13:00:29 +0900

    r3q: Add media C2 from android 11 vendor

--- device.mk
+++ device.mk
@@ -238,6 +238,12 @@ PRODUCT_PACKAGES += \
     android.hardware.media.omx@1.0-impl \
     android.hardware.media.omx@1.0-service
 
+# Media C2
+PRODUCT_PACKAGES += \
+    android.hardware.media.c2@1.0.vendor \
+    libcodec2_vndk.vendor \
+    libcodec2_hidl@1.0.vendor
+
 # Neuralnetworks
 PRODUCT_PACKAGES += \
     android.hardware.neuralnetworks@1.3.vendor
