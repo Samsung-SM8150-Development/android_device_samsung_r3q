8713072e8d5a88a5fa089b91280d6e616efc48b4
Michael Bestas <mkbestas@lineageos.org>
2023-11-18 10:30:32 +0900

    r3q: Set androidboot.init_fatal_reboot_target=recovery
    * Don't crash to bootloader, users can access pstore in recovery in order to debug the issue.
    
    Change-Id: I33e91fa31477785ba9c14a56d1c7ee9110fcc0c9

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -51,6 +51,7 @@ BOARD_BOOT_HEADER_VERSION := 1
 BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_CMDLINE := console=null androidboot.hardware=qcom androidboot.console=ttyMSM0 androidboot.memcg=1 lpm_levels.sleep_disabled=1 video=vfb:640x400,bpp=32,memsize=3072000 msm_rtb.filter=0x237 service_locator.enable=1 swiotlb=2048 androidboot.usbcontroller=a600000.dwc3 firmware_class.path=/vendor/firmware_mnt/image loop.max_part=7
 BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
+BOARD_KERNEL_CMDLINE += androidboot.init_fatal_reboot_target=recovery
 BOARD_KERNEL_IMAGE_NAME := Image-dtb
 BOARD_KERNEL_OFFSET := 0x00008000
 BOARD_KERNEL_PAGESIZE := 4096
